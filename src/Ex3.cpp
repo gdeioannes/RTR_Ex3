dbfb8db4892aa79e7205c32328722052aff61366
Gabriel De Ioannes Becker <gdeioannes@gmail.com>
2019-12-10 16:18:00 +0100

    Just the repossitory not work yet

---
+++ src/Ex3.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Ex3.cpp
+// Author      : Gabriel De Ioannes
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Real Time Rendering AKA Geometric Computing!!!" << endl; // prints !!!Hello Real Time Rendering AKA Geometric Computing!!!
+	return 0;
+}
